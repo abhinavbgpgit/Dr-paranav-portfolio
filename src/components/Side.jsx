6056809cbcf51e9a5d359de21e5138ce689eb178
abhinavbgpgit <abhinavbgp@gmail.com>
2025-08-12 10:25:20 +0530

    logo change

--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { FiDownload } from "react-icons/fi";
 import { IoLogoWhatsapp } from "react-icons/io";
-import profile_pic from '../assets/general/dr_pranav.png'
-import logo from '../assets/logo/physiotherapy_logo.png'
+import profile_pic from "../assets/general/dr_pranav.png";
+import logo from "../assets/logo/logo_physio.png";
 export default function Side() {
   return (
-     <div className="bg-white shadow-md rounded-lg p-4 mt-16 h-[600px] relative min-w-[250px]">
+    <div className="bg-white shadow-md rounded-lg p-4 mt-16 h-[600px] relative min-w-[250px]">
       {/* Header */}
       <div className="flex items-center justify-between mb-4">
         <div>
@@ -19,57 +19,69 @@ export default function Side() {
           </svg>
         </div> */}
       </div>
-        <div className='h-[220px]'></div>
+      <div className="h-[220px]"></div>
       {/* Profile Image */}
       <img
         src={profile_pic}
         alt="Dr. Robert Gibbons"
         className="w-full h-64 object-cover mb-4 absolute top-11 left-0"
-        style={{ clipPath: 'polygon(0 14%, 100% 5%, 100% 93%, 0% 100%)' }}
+        style={{ clipPath: "polygon(0 14%, 100% 5%, 100% 93%, 0% 100%)" }}
       />
 
-       <img
+      <img
         src={logo}
         alt="Dr. Robert Gibbons"
-        className=" h-12 object-cover mb-4 absolute  left-[10px] top-[270px]"
-        
+        className=" h-12 object-cover mb-4 absolute rounded-full border-2  left-[12px] top-[270px]"
       />
 
       {/* Details */}
       <ul className=" ml-5">
-        <li className=' h-10 border-l-2 '></li>
-        <li className=" border-l-2 flex items-center bg-red-300 py-2 relative">
-        <span className="absolute top-0 left-[-5px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
-  <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
-</span>
-          <span className="font-semibold text-xs ml-[-5px]">Name : </span><span className=" text-xs ml-1"> Pranav Kumar</span> 
+        <li className=" h-10 border-l-2 "></li>
+        <li className=" border-l-2 flex items-center  py-2 relative">
+          <span className="absolute top-0 left-[-7px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
+          </span>
+          <span className="font-semibold text-xs ml-[-5px]">Name : </span>
+          <span className=" text-xs ml-1"> Pranav Kumar</span>
         </li>
-        <li className="flex items-center">
-          <span className="inline-block bg-blue-500 rounded-full w-3 h-3 mr-2"></span>
-          <span className="font-semibold text-xs">Birthday :</span>
-          <span className="text-xs ml-1">06 Dec 1975</span>
+
+        <li className=" border-l-2 flex items-center  py-2 relative">
+          <span className="absolute top-0 left-[-7px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
+          </span>
+          <span className="font-semibold text-xs ml-[-5px]">Birthday : </span>
+          <span className=" text-xs ml-1"> 06 Dec 1975</span>
         </li>
-        <li className="flex items-center">
-          <span className="inline-block bg-blue-500 rounded-full w-3 h-3 mr-2"></span>
-          <span className="font-semibold text-xs">Job :</span>
-          <span className="text-xs ml-1">Physiotherapist</span>
+
+        <li className=" border-l-2 flex items-center  py-2 relative">
+          <span className="absolute top-0 left-[-7px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
+          </span>
+          <span className="font-semibold text-xs ml-[-5px]">Job : </span>
+          <span className=" text-xs ml-1"> Physiotherapist</span>
         </li>
-        <li className="flex items-center">
-          <span className="inline-block bg-blue-500 rounded-full w-3 h-3 mr-2"></span>
-          <span className="font-semibold text-xs">Email :</span>
-          <span className="text-xs ml-1">pranav@gmail.com</span>
+
+        <li className=" border-l-2 flex items-center  py-2 relative">
+          <span className="absolute top-0 left-[-7px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
+          </span>
+          <span className="font-semibold text-xs ml-[-5px]">Email : </span>
+          <span className=" text-xs ml-1"> pranav@gmail.com</span>
         </li>
-        <li className="flex items-center">
-          <span className="inline-block bg-blue-500 rounded-full w-3 h-3 mr-2"></span>
-          <span className="font-semibold text-xs">Skype :</span>
-          <span className="text-xs ml-1">paranav85</span>
+
+        <li className=" border-l-2 flex items-center  py-2 relative">
+          <span className="absolute top-0 left-[-7px] inline-block bg-blue-500 rounded-full w-3 h-3 mr-2 relative">
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-full w-2 h-2"></span>
+          </span>
+          <span className="font-semibold text-xs ml-[-5px]">Skype : </span>
+          <span className=" text-xs ml-1"> paranav85</span>
         </li>
       </ul>
 
       {/* Download CV Button */}
       <button className="mt-6 bg-white border-2 text-xs  px-3 py-2 rounded-full flex items-center space-x-2 hover:bg-gray-200 transition duration-300">
         <span>Message on Whatsapp</span>
-        <IoLogoWhatsapp size={18} className='text-green-500'/>
+        <IoLogoWhatsapp size={18} className="text-green-500" />
       </button>
     </div>
   );
